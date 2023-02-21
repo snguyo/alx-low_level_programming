a1817bf77aa02aef6355468d1f61541c1d402bf3
Samuel N <snguyo@gmail.com>
2023-02-21 22:03:45 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,15 @@
 #include"main.h"
 
 /**
- * main - Entry point
- *
- * Description: prints _putchar using putchar prototype
- *
- * Return: Always 0 (Success)
+ * print_alphabet - utilizes on the _putchar function to print
+ *                 the alphabet a - z
 */
 
-int main(void)
+void print_alphabet(void)
 {
-	char str[] = "_putchar";
 	int ch;
 
-	for (ch = 0; ch < 8; ++ch)
-		_putchar(str[ch]);
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
 	_putchar('\n');
-
-	return (0);
 }
