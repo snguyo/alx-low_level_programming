a0366b0d7e93150c639c9534ddf9a04a5081d4ea
snguyo <snguyo@gmail.com>
2023-03-10 03:18:40 -0800

    Static Libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,3 +23,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 #endif
+
